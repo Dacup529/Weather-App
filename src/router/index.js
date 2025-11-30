23c369eba83acd0c528dfae484e56909fcd1d73c
Reese Parker <112765638+Dacup529@users.noreply.github.com>
2025-11-30 16:10:53 -0500

    Updating files again before deploying to GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from "vue-router";
+import {createRouter, createWebHashHistory} from "vue-router";
 import Home from "../views/Home.vue";
 import Hourly from "../views/Hourly.vue";
 import FiveDay from "../views/FiveDay.vue";
@@ -10,6 +10,6 @@ const routes = [
 ];
 
 export default createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 });
